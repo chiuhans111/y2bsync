509ac11e4fb56f43bac5ea1598dc05aa327e68f5
Hans Chiu <chiuhans111@gmail.com>
2018-07-10 17:30:10 +0800

    port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const serve = require('koa-static')
 const Koa = require('koa')
 const app = new Koa()
- 
+var port = process.env.PORT || 8080;
 // response
 app.use(serve('./pub'))
  
-app.listen(8080)
+app.listen(port)
